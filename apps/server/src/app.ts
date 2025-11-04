d1da7cf7a20ebff61b7daa70e5dda3c8c952cd9d
Patryk <patryk.kwasek@ing.com>
2025-11-04 20:47:20 +0100

    Added basic login with JWT Bearer Token + added HelmetJS

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -2,6 +2,8 @@ import cors from 'cors';
 import 'dotenv/config';
 import express, { json, urlencoded } from 'express';
 import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import loginRoute from './routes/loginRoute';
 import registrationRoutes from './routes/registrationRoutes';
 
 const app = express()
@@ -21,9 +23,11 @@ const limiter = rateLimit({
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: false }));
+app.use(helmet());
 
 // Routes
 app.use('/registration', limiter, registrationRoutes);
+app.use('/login', limiter, loginRoute);
 
 app.listen(port, () => {
   console.log(`Server running at http://127.0.0.1:${port}/`);
