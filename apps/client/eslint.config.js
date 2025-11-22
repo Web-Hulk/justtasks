3245154e801ce60fb0ef7932e7028e95da936a1c
Patryk <patryk.kwasek@ing.com>
2025-11-22 16:29:04 +0100

    Added Storybook setup - ready to use in next phase of the app development

--- apps/client/eslint.config.js
+++ apps/client/eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 // eslint.config.js
 import js from '@eslint/js';
 import pluginQuery from '@tanstack/eslint-plugin-query';
@@ -38,4 +41,4 @@ export default tseslint.config({
     'react/jsx-uses-react': 'off',
     'react/prop-types': 'off'
   }
-});
+}, storybook.configs["flat/recommended"]);
